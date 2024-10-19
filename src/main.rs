c2a7e6b64179b015cd79d732fbae5bf91a6571a8
Syed Ibna Zubayear <47857423+Zubayear@users.noreply.github.com>
2024-10-19 21:33:48 +0600

    meh

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 use std::env;
+use std::net::Ipv4Addr;
+use std::sync::Arc;
 
+use diameter::avp::address::Value::IPv4;
+use diameter::avp::flags::M;
+use diameter::avp::Address;
+use diameter::avp::Enumerated;
+use diameter::avp::Identity;
+use diameter::avp::UTF8String;
+use diameter::avp::Unsigned32;
+use diameter::dictionary::{self, Dictionary};
+use diameter::flags;
+use diameter::transport::DiameterClient;
+use diameter::transport::DiameterClientConfig;
+use diameter::{ApplicationId, CommandCode, DiameterMessage};
 use log::{debug, info, warn};
 
+use is_empty::IsEmpty;
 use rdkafka::client::ClientContext;
 use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
 use rdkafka::consumer::stream_consumer::StreamConsumer;
 use rdkafka::consumer::{CommitMode, Consumer, ConsumerContext, Rebalance};
 use rdkafka::error::KafkaResult;
-use rdkafka::message::{Headers, Message};
+use rdkafka::message::Message;
 use rdkafka::topic_partition_list::TopicPartitionList;
 use serde::{Deserialize, Serialize};
 
@@ -40,7 +55,7 @@ async fn consume_and_print(brokers: &str, group_id: &str, topics: &[&str]) {
         .set("bootstrap.servers", brokers)
         .set("enable.partition.eof", "false")
         .set("session.timeout.ms", "6000")
-        .set("enable.auto.commit", "false")
+        .set("enable.auto.commit", "true")
         //.set("statistics.interval.ms", "30000")
         .set("auto.offset.reset", "latest")
         .set_log_level(RDKafkaLogLevel::Debug)
@@ -53,7 +68,7 @@ async fn consume_and_print(brokers: &str, group_id: &str, topics: &[&str]) {
 
     loop {
         match consumer.recv().await {
-            Err(e) => warn!("Kafka error: {}", e),
+            Err(e) => warn!("Kafka error {}", e),
             Ok(m) => {
                 let payload = match m.payload_view::<str>() {
                     None => "",
@@ -64,51 +79,121 @@ async fn consume_and_print(brokers: &str, group_id: &str, topics: &[&str]) {
                     }
                 };
 
-                info!("key: '{:?}', payload: '{}', topic: {}, partition: {}, offset: {}, timestamp: {:?}",
-                      m.key(), payload, m.topic(), m.partition(), m.offset(), m.timestamp());
-
-                let _my_message = match serde_json::from_str::<MyMessage>(payload) {
+                let my_message = match serde_json::from_str::<MyMessage>(payload) {
                     Ok(t) => t,
                     Err(e) => {
                         warn!("error {}", e);
-                        MyMessage {
-                            name: "".to_string(),
-                        }
+                        MyMessage { name: None }
                     }
                 };
 
-                if let Some(headers) = m.headers() {
-                    for header in headers.iter() {
-                        info!("  Header {:#?}: {:?}", header.key, header.value);
-                    }
-                }
-                // consumer.commit_message(&m, CommitMode::Async).unwrap();
+                info!("message: {:?}", my_message);
 
-                if let Err(commit_err) = consumer.commit_message(&m, CommitMode::Async) {
-                    warn!("Error committing message: {}", commit_err);
-                } else {
-                    info!("message committed");
+                if !my_message.is_empty() {
+                    if let Err(commit_err) = consumer.commit_message(&m, CommitMode::Async) {
+                        warn!("Error committing message: {}", commit_err);
+                    } else {
+                        info!("message committed");
+                    }
                 }
             }
         };
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, IsEmpty)]
 struct MyMessage {
-    name: String,
+    name: Option<String>,
 }
 
 #[tokio::main]
 async fn main() {
-    env::set_var("RUST_LOG", "debug");
+    env::set_var("RUST_LOG", "info");
     env_logger::init();
 
-    info!("Yo bro 'Talk Tuah' is on");
+    debug!("Application started");
 
     let brokers = "localhost:9092";
     let group_id = "group.id";
     let topics = &["the_doors"];
-
     consume_and_print(brokers, group_id, topics).await;
+    info!("yo bro 'Talk Tuah' is on");
+
+    let dict = Dictionary::new(&[&dictionary::DEFAULT_DICT_XML]);
+    let dict = Arc::new(dict);
+
+    let client_config = DiameterClientConfig {
+        use_tls: false,
+        verify_cert: false,
+    };
+
+    let mut client = DiameterClient::new("localhost:3868", client_config);
+    match client.connect().await {
+        Ok(mut handler) => {
+            let dict_ref = Arc::clone(&dict);
+            tokio::spawn(async move {
+                DiameterClient::handle(&mut handler, dict_ref).await;
+            });
+
+            send_cer(&mut client, Arc::clone(&dict)).await;
+
+            send_ccr(&mut client, Arc::clone(&dict)).await;
+        }
+        Err(e) => {
+            warn!("Could not connect to diameter server: {}", e);
+        }
+    }
+}
+
+async fn send_cer(client: &mut DiameterClient, dict: Arc<Dictionary>) {
+    let seq_num = client.get_next_seq_num();
+    let mut cer = DiameterMessage::new(
+        CommandCode::CapabilitiesExchange,
+        ApplicationId::Common,
+        flags::REQUEST,
+        seq_num,
+        seq_num,
+        dict,
+    );
+    cer.add_avp(264, None, M, Identity::new("host.example.com").into());
+    cer.add_avp(296, None, M, Identity::new("realm.example.com").into());
+    cer.add_avp(
+        257,
+        None,
+        M,
+        Address::new(IPv4(Ipv4Addr::new(127, 0, 0, 1))).into(),
+    );
+    cer.add_avp(266, None, M, Unsigned32::new(35838).into());
+    cer.add_avp(269, None, M, UTF8String::new("diameter-rs").into());
+
+    let resp = client.send_message(cer).await.unwrap();
+    let cea = resp.await.unwrap();
+    info!("Received rseponse: {}", cea);
+}
+
+async fn send_ccr(client: &mut DiameterClient, dict: Arc<Dictionary>) {
+    let seq_num = client.get_next_seq_num();
+    let mut ccr = DiameterMessage::new(
+        CommandCode::CreditControl,
+        ApplicationId::CreditControl,
+        flags::REQUEST,
+        seq_num,
+        seq_num,
+        dict,
+    );
+    ccr.add_avp(264, None, M, Identity::new("host.example.com").into());
+    ccr.add_avp(296, None, M, Identity::new("realm.example.com").into());
+    ccr.add_avp(263, None, M, UTF8String::new("ses;12345888").into());
+    ccr.add_avp(416, None, M, Enumerated::new(1).into());
+    ccr.add_avp(415, None, M, Unsigned32::new(1000).into());
+    ccr.add_avp(
+        1228,
+        Some(10415),
+        M,
+        Address::new(IPv4(Ipv4Addr::new(127, 0, 0, 1))).into(),
+    );
+
+    let resp = client.send_message(ccr).await.unwrap();
+    let cca = resp.await.unwrap();
+    info!("Received rseponse: {}", cca);
 }
